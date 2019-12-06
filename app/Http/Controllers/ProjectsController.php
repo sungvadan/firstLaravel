93c8780fde3d86c6e82dbe6daeabad94dd9d9c89
sungvadan <svd.phan@gmail.com>
2019-12-06 16:28:04 +0100

    eloquent

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('projects.index', [
+            'projects' => $projects
+        ]);
+    }
+}
---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('title', 'project')
+
+@section('content')
+    <h1>Projects</h1>
+    <ul>
+        @foreach($projects as $project)
+            <li>{{ $project->title }}</li>
+        @endforeach
+    </ul>
+@endsection
+
