001d5fedb607d61b333ec17927327925062be306
sungvadan <svd.phan@gmail.com>
2019-12-09 21:20:16 +0100

    first eloquent relationships

--- app/Project.php
+++ app/Project.php
@@ -13,4 +13,9 @@ class Project extends Model
 //        'title',
 //        'description',
 //    ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,5 +3,14 @@
 @section('container')
     <h1 class="title">{{ $project->title }}</h1>
     <div class="content">{{ $project->description }}</div>
+
+    @if ($project->tasks->count())
+        List of tasks :
+        <ol type="1">
+            @foreach ($project->tasks as $task)
+                <li>{{ $task->description }}</li>
+            @endforeach
+        </ol>
+    @endif
     <a href="/projects/{{ $project->id }}/edit">Edit</a>
 @endsection
